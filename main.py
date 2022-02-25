903dac469682d7998934f5da9fb1369a35ec5515
Daniel Zajork <dan@mr-pro-bot.local>
2022-02-25 18:27:03 -0500

    first pass

--- main.py
+++ main.py
@@ -1,29 +1,85 @@
 #!/usr/bin/python3
 import json
 import sys
-import os
-import concurrent.futures
 import requests
+import tldextract
 from tqdm import tqdm
+import concurrent.futures
 from argparse import ArgumentParser
 import re
 
-
-
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def clean_matched_results(matches):
+    endpoints = []
+
+    for match in matches:
+        endpoint = match.group(0).lstrip('"')
+        endpoint = endpoint.rstrip('"')
+        endpoint = endpoint.lstrip("'")
+        endpoint = endpoint.rstrip("'")
+
+        endpoints.append(endpoint)
+
+    # return unique results
+    return list(set(endpoints))
+
+def extract_double_quoted_apis(text):
+
+    regex = r'"(?<=(\"|\'|\`))\/api\/[a-zA-Z0-9_?&=\/\-\#\.]*(?=(\"|\'|\`))"'
+    matches = re.finditer(regex, text, re.MULTILINE)
+
+    return clean_matched_results(matches)
+
+def extract_single_quoted_apis(text):
+
+    regex = r"'(?<=(\"|\'|\`))\/api\/[a-zA-Z0-9_?&=\/\-\#\.]*(?=(\"|\'|\`))'"
+    matches = re.finditer(regex, text, re.MULTILINE)
+
+    return clean_matched_results(matches)
+
+def build_get_urls(url, endpoints):
+
+    get_endpoints = []
+
+    scheme = "http://"
+    if url.startswith("https://"):
+        scheme = "https://"
+
+    extracted_result = tldextract.extract(url)
+    full_domain = f"{extracted_result.subdomain}.{extracted_result.domain}.{extracted_result.suffix}"
+
+    for endpoint in endpoints:
+        api_endpoint = f"{scheme}{full_domain}{endpoint}"
+        get_endpoints.append(api_endpoint)
+
+    return get_endpoints
 
 
 def scan(url, threads, output):
     
     response = requests.get(url, timeout=5, allow_redirects=False, verify=False)
+    text = response.text
 
-    pattern = re.compile(r"'(?<=(\"|\'|\`))\/api\/[a-zA-Z0-9_?&=\/\-\#\.]*(?=(\"|\'|\`))'")
-    matches = pattern.match(response.text)
+    endpoints = []
 
-    for match in matches:
-        print(match)
+    endpoints.extend(extract_single_quoted_apis(text))
+    endpoints.extend(extract_double_quoted_apis(text))
 
+    get_endpoints = build_get_urls(url, endpoints)
+
+    for e in get_endpoints:
+        response = requests.get(e, timeout=5, verify=False)
+        length = len(response.content)
+       
+        result = {
+            "status_code": response.status_code,
+            "length": length,
+            "url": e
+        }
+
+        print(f"[*] {response.status_code} : {length} : {e}")
 
 def main():
     """
@@ -31,9 +87,8 @@ def main():
     """
     parser = ArgumentParser()
     parser.add_argument("-u", "--url", dest="url", help="url to target")
-    parser.add_argument("-w", "--word-list", dest="word_list", help="custom word list")
-    parser.add_argument("-t", "--threads", dest="threads", help="number of threads")
     parser.add_argument("-o", "--out", dest="output", help="file to output json")
+    parser.add_argument("-t", "--threads", dest="threads", help="number of threads")
     
     args = parser.parse_args()
 
